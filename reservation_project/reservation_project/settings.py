0e3dc97452879735a45ac17815f91cbee9b16080
Gueremy <gueremybtos@gmail.com>
2025-12-23 20:15:05 -0300

    Fix: Restore corrupted settings.py

--- reservation_project/reservation_project/settings.py
+++ reservation_project/reservation_project/settings.py
@@ -41,7 +41,120 @@ else:
     CSRF_TRUSTED_ORIGINS = ['https://terratokenx.onrender.com']
 
 # Application definition
-# ... (rest of the file until email settings)
+
+INSTALLED_APPS = [
+    'admin_interface',
+    'colorfield',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'import_export',  # Añadir django-import-export
+    'reservation_project.booking',       # Ruta completa a la app booking
+    'django.contrib.humanize',
+    'widget_tweaks',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Middleware para servir archivos estáticos
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'reservation_project.reservation_project.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'booking', 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'reservation_project.reservation_project.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/4.x/ref/settings/#databases
+
+# Creamos una ruta persistente para la base de datos en Render
+DATA_DIR = BASE_DIR / 'data'
+DATA_DIR.mkdir(exist_ok=True)
+
+DATABASES = {
+    # La configuración por defecto es para producción, usando la variable de entorno DATABASE_URL.
+    # dj-database-url se encargará de todo.
+    'default': dj_database_url.config(default=f"sqlite:///{DATA_DIR / 'db.sqlite3'}", conn_max_age=600)
+}
+
+
+
+# Password validation
+# https://docs.djangoproject.com/en/4.x/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+# Internationalization
+# https://docs.djangoproject.com/en/4.x/topics/i18n/
+
+LANGUAGE_CODE = 'es-cl'
+
+TIME_ZONE = 'America/Santiago'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+USE_THOUSAND_SEPARATOR = True
+NUMBER_GROUPING = 3
+DECIMAL_SEPARATOR = ','
+THOUSAND_SEPARATOR = '.'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.x/howto/static-files/
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Directorio donde se recopilarán los archivos estáticos
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.x/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#mercado pago configuración
+MERCADO_PAGO_PUBLIC_KEY = env('MERCADO_PAGO_PUBLIC_KEY')
+MERCADO_PAGO_ACCESS_TOKEN = env('MERCADO_PAGO_ACCESS_TOKEN')
 
 # Ejemplo para Gmail
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
